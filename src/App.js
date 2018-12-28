36ef895eb40a8083e9390d00f6046ce9835d3e38
lakshay14 <laksh1013shar@gmail.com>
2018-12-29 02:11:55 +0530

    MarkDown Previewer Running git status

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import sample from './sample.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+let marked=require('marked');
+
+class App extends Component 
+{
+  constructor(props) 
+  {
+    super(props);
+    this.state = 
+    {
+      value:sample
+     
+    }
   }
+
+//-------------------------------------------------------------------------------------
+  
+  handleChange = (e)=> 
+  {
+    this.setState({
+      value: e
+    });
+    console.log(this.state.userInput);
+  }
+
+  getMarkdownText(ele) 
+  {
+    var rawMarkup = marked(ele, {sanitize: true});
+    return { __html: rawMarkup};
+  }
+
+//--------------------------------------------------------------------------------------
+render() 
+{
+  let {value} = this.state
+  return (
+    <div >
+       
+        <div className="card-panel #212121 grey darken-4">
+          <h1 id = "one" className="center yellow-text bold">__^^MarkdownPreviewer^^__</h1>
+        </div>
+
+        
+       <div className="col s6 ">
+       <textarea id="editor" className="card-panel #e6ee9c lime lighten-3"
+          value={value}
+          placeholder="Input"
+          rows="35"
+          // cols="10"
+          onChange={(e)=>this.handleChange(e.target.value)}
+          /> 
+      </div>
+
+      <h4 className="center card-panel #dd2c00 deep-orange accent-4 ">$!-Markdown Result-!$</h4>
+
+      <div className="col s6 card-panel #dcedc8 light-green lighten-4">
+        <div id="preview" dangerouslySetInnerHTML={{__html: marked(value)}} />
+      </div>
+     
+    </div>
+  );
+}
 }
 
 export default App;
---
+++ src/sample.js
@@ -0,0 +1,9 @@
+module.exports = 
+`Markdown syntax typed into the first pane will be automatically converted to HTML and rendered in the second pane. Go ahead, give it a try!  \n\n#### Headings:  \n# Heading 1\n## Heading 2\n---\n#### Hyperlink:   \n[Google](https://google.com)  \n\n---\n#### Inline code:\nFrom the console, run \`console.log('Hello world!');\` to see the output.    \n\n---\n\n#### Code block:\n\`\`\`javascript\nfunction sayHello() {\n  return 'Hello world!';\n}\n\`\`\`\n---\n\n#### List items:\n1. Ordered list item 1  \n2. Ordered list item 2  \n\n\n- Unordered list item  \n- Unordered list item  \n\n---\n\n#### Blockquote:  \n> Part of the inhumanity of the computer is that, once it is competently programmed and working smoothly, it is completely honest. -Isaac Asimov \n\n---\n#### Image:  \n![alt text](https://avatars2.githubusercontent.com/u/9892522?s=100&v=4 'freeCodeCamp Avatar')  \n\n---\n#### Formatted text:\nAs **bolded** or *emphasized* text.`
+;
+
+
+
+// '# I am an h1!\n\n## I am an h2!\n\n###### I am just a little guy!\n\n[I\'m a link](https://www.google.com) and this is some code:\n `if (betweenTwoBackTicks) { isCode = true }`\n\nHow about a code block?\n\`\`\`\n<html>\n  <head>\n  </head>\n<body>\n<h1>Welcome to the dOcUmEnT</h1>\n</body>\n</html>\n\`\`\`\n > Block Quotes!\n\n_Maybe_ we should make a **list** of all this stuff?\n- So much stuff\n   - Even more stuff\n      - The most things\n\nBuilt with:\n![React Logo w/ Text](https://goo.gl/Umyytc)\n\nRendered by **marked**.';
+
+
